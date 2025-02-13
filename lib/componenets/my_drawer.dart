f61bd9333b38563e943ebb5a4224ee006ee21fca
AdnanAdu <adnanpe5@gmail.com>
2025-02-13 20:17:08 +0530

    added Customized Drawer Widget

---
+++ lib/componenets/my_drawer.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:music_player/pages/settings_page.dart';
+
+class MyDrawer extends StatelessWidget {
+  const MyDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      child: Column(
+        children: [
+          DrawerHeader(
+            child: Center(
+              child: Icon(
+                Icons.music_note,
+                size: 40,
+                color: Theme.of(context).colorScheme.inversePrimary,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 25, top: 25),
+            child: ListTile(
+              leading: Icon(Icons.home),
+              title: Text("H O M E"),
+              onTap: () => Navigator.pop(context),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 25, top: 0),
+            child: ListTile(
+              leading: Icon(Icons.settings),
+              title: Text("S E T T I N G S"),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => SettingsPage()));
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:music_player/componenets/my_drawer.dart';
 
 class HomePage extends HookWidget {
   const HomePage({super.key});
@@ -8,6 +9,12 @@ class HomePage extends HookWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.surface,
+      appBar: AppBar(
+        title: Text("P L A Y L I S T"),
+      ),
+      drawer: MyDrawer(
+
+      ),
     );
   }
 }
